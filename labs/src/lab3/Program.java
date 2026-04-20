b96fa7e2e7db319ca402526a9721df2f077606f9
jessica watson <jessica.watson@yorkshirewater.co.uk>
2026-04-20 15:05:07 +0100

    updates

--- labs/src/lab3/Program.java
+++ labs/src/lab3/Program.java
@@ -1,10 +1,17 @@
 package lab3;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int age = getInt("How old are you");
+		System.out.println(age);
+	}
+	public static int getInt(String prompt){
+		System.out.printf(prompt + ": ");
+		Scanner s = new Scanner(System.in);
+		return s.nextInt();
 	}
 
 }
