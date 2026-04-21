1f33892bca628ad67b3eb26b99ad6eecb4d30d00
jwatsonyw <jessica.watson@yorkshirewater.co.uk>
2026-04-21 13:20:29 +0100

    update

--- labs/src/lab4/Program2.java
+++ labs/src/lab4/Program2.java
@@ -5,14 +5,15 @@ package lab4;
 public class Program2 {
 
 	public static void main(String[] args) {
-	int[] numbers = {1,3,-5,7,0,4,6,8};
+	
 		
-		int sum = 0;
-		for(int number : numbers) {
-		sum += number;
+		int[] numbers = {1,3,-5,7,0,4,6,8};
+		int min = numbers[1]; //Change to go through numbers
+		if(numbers[1] < min)
+			min = numbers[0];
+	
+		
+			System.out.println("The minimum number is: " + min);
 	}
-		double average = (double)sum / numbers.length;
-		System.out.println("The average is: " + average);
-	}
-
+	
 }
