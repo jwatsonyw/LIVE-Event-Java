31e9e1dbd864b78003889e2bbe22b1721dfba4a7
jwatsonyw <jessica.watson@yorkshirewater.co.uk>
2026-04-22 10:31:00 +0100

    update

--- labs/src/lab6/Game.java
+++ labs/src/lab6/Game.java
@@ -1,6 +1,5 @@
 package lab6;
 
-
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
@@ -8,67 +7,51 @@ import java.util.Timer;
 import javax.swing.*;
 
 public class Game extends Canvas {
-	public static void main(String[] args) {
-		new Game();
-	}
+    public static void main(String[] args) {
+        new Game();
+    }
 
-// create an array of 3 balls 
-// call this array balls
-	Ball[] balls = new Ball[3];
+    World world;
+    
+    public Game() {
+        world = new World();
+        
+        JFrame frame = new JFrame();
+        
+        this.setSize(650, 500);
+        frame.add(this);
+        frame.pack();
+        frame.setVisible(true);
 
-	Game() {
-		
-		balls[0] = new Ball(10,10,50,50,1,1);
-		balls[1] = new Ball(20,5,30,50,3,2);
-		balls[2] = new Ball(30,10,50,30,2,3);
-		
-		JFrame frame = new JFrame();
-		this.setSize(650, 500);
-		frame.add(this);
-		frame.pack();
-		frame.setVisible(true);
+        Timer t = new Timer();
+        TimerTask tt = new TimerTask() {
+            @Override
+            public void run() {
+                draw();
+            }
+        };
 
-		Timer t = new Timer();
-		TimerTask tt = new TimerTask() {
-			@Override
-			public void run() {
-				draw();
-			}
-		};
+        t.schedule(tt, 0, 50);
 
-		t.schedule(tt, 0, 50);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                t.cancel();
+                tt.cancel();
+            }
+        });
+    }
 
-		frame.addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent e) {
-				t.cancel();
-				tt.cancel();
-			}
-		});
-	}
+    public void draw() {
+        this.repaint();
+    }
 
-	public void draw() {
-		this.repaint();
-	}
-
-	public void paint(Graphics g) {
-		// call the move() method of each balls
-		// Tip: use an enhanced for loop to pick
-		// each ball in the balls array.
-
-// draw each ball in balls array
-// Tip: See the example below
-		g.setColor(Color.RED);
-		g.fillRect(10, 10, 550, 300);
-		g.setColor(Color.blue);
-		
-		for(Ball ball : balls) {
-			ball.move();
-			g.fillOval(ball.x, ball.y, ball.w, ball.h);
-		}
-		
-		
-
-		
-	}
+    public void paint(Graphics g) {
+        g.setColor(World.colour);
+        g.fillRect(World.x, World.y, World.w, World.h);
+        g.setColor(Color.blue);
+        
+        world.draw(g);
+        
+    }
 }
