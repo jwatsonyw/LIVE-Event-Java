bee9816f2604f7c9ab71c4c66260e36626c51824
jwatsonyw <jessica.watson@yorkshirewater.co.uk>
2026-04-22 14:54:48 +0100

    updates

--- labs/src/lab9/Program.java
+++ labs/src/lab9/Program.java
@@ -1,10 +1,44 @@
 package lab9;
 
+import java.io.Console;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int id = 123;
+		String owner = "Jess";
+	    double balance = 100;
+  
 	}
+    public String GetDetails()
+    {
+       return $"ID: {id}, Owner: {owner}, Balance: {balance}";
+        }
+        
+    public void Deposit(double amount)
+    {
+    	if (amount > 0)
+    	{
+    		balance += amount;
+    		    	}
+    }
+    public void Withdraw(double amount)
+    {
+    	if (amount > 0 && amount <=balance)
+    	{
+    		balance -= amount;
+    	}
+    	else
+    	{
+    		throw new ArgumentException("Not enough money in account" + amount);
+    		    	}
+    }
+    public void Close()
+    {
+    	Console.writeLine("Account 123 is closed!");
+    
+    }
 
+	
 }
