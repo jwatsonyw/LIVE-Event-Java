55ee26fa358de71b16330f1584713a6b7c811a46
jwatsonyw <jessica.watson@yorkshirewater.co.uk>
2026-04-22 10:31:31 +0100

    update

--- labs/src/lab6/World.java
+++ labs/src/lab6/World.java
@@ -1,10 +1,32 @@
 package lab6;
 
-public class World {
+import java.awt.Color;
+import java.awt.Graphics;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+public class World{
+	public static int x=50, y=40, w=500, h=400;
+	public static Color colour = Color.GREEN;
+	
+	public static int bottom() {
+		return y+h;
 	}
-
+	public static int right() {
+		return x+w;
+	}
+	Ball[] balls = new Ball[3];
+	
+	public World() {
+		balls[0] = new Ball(30,40,50,50,5,5);
+		balls[1] = new Ball(40,35,30,50,3,6);
+		balls[2] = new Ball(30,40,50,30,6,3);
+	}
+	
+	public void draw(Graphics g) {
+		for(Ball ball : balls) {
+			ball.move();
+			g.fillOval(ball.x, ball.y, ball.w, ball.h);
+		}
+	}
+		
 }
