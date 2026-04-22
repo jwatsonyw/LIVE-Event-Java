567e8b9103989bfcbe79ddaf66a95c1dd10bc2ee
jwatsonyw <jessica.watson@yorkshirewater.co.uk>
2026-04-22 13:56:07 +0100

    update
    Added exception handling to readMyFile method and main method.

--- labs/src/lab8/Exceptions.java
+++ labs/src/lab8/Exceptions.java
@@ -1,10 +1,46 @@
 package lab8;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
 public class Exceptions {
 
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			readMyFile("c:\\qa\\tx100\\server.js");
+		} catch (Exception e) {
+			System.out.println(e.getStackTrace());
+			return;
+		}
+		finally {
+			System.out.println("Runs no matter what!");  // Death, Tax, Finally!
+		}
+		
+		System.out.println("Doing other things!");
+	}
+
+
+	private static void readMyFile(String fileName) throws IllegalArgumentException
+	{
+		try {
+			File myObj = new File(fileName);
+
+
+			Scanner myReader = new Scanner(myObj);
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				System.out.println(data);
+			}
+		} catch (Exception e) {
+			System.out.println("readMyFile handled the exception");
+			throw new IllegalArgumentException("File not found");
+		}
+
 
 	}
+
 
 }
