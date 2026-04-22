8669a290fd6ce125fab9801d3a240e4155fb81f3
jwatsonyw <jessica.watson@yorkshirewater.co.uk>
2026-04-22 10:30:01 +0100

    updated
    Refactor Ball class to improve movement logic and encapsulate properties.

--- labs/src/lab6/Ball.java
+++ labs/src/lab6/Ball.java
@@ -1,12 +1,9 @@
 package lab6;
 
 public class Ball {
-
-	public static void main(String[] args) {
 	
-public class Ball {
-	int x,y,w,h, dirX, dirY;
-
+	public int x, y, w, h, dirX, dirY;
+	
 	public Ball(int x, int y, int w, int h, int dirX, int dirY) {
 		this.x = x;
 		this.y = y;
@@ -16,12 +13,28 @@ public class Ball {
 		this.dirY = dirY;
 	}
 
+
 	public void move() {
+		// x y w h
 		x += dirX;
 		y += dirY;
-		
-		if(x < 0 || x > 400) dirX = -dirX;
-		if(y < 0 || y > 200) dirY = -dirY;
-	}
+		if (x < World.x ) {
+			x = World.x;
+			dirX = -dirX;
+		}
+		if (y < World.y) {
+			y = World.y;
+			dirY = -dirY;
+		}
 
+
+		if (y + h > World.bottom()) {
+			y -= dirY;
+			dirY = -dirY;
+		}
+		if (x + w > World.right()) {
+			x -= dirX;
+			dirX = -dirX;
+		}
+	}
 }
