bec981bc3e05b485bc98f97d3c142226b26b5a6f
jwatsonyw <jessica.watson@yorkshirewater.co.uk>
2026-04-23 15:22:19 +0100

    updates

---
+++ labs/src/lab12/Animal.java
@@ -0,0 +1,23 @@
+package lab12;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		
+		public class Animal{
+		String animalType;
+		String name;
+		}
+		
+		enum AnimalType{
+			Fish,
+			Amphibian,
+			Reptil,
+			Mammal,
+			Bird,
+			Invertebrate
+		}
+
+	}
+
+}
